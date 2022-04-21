c587a9588c1763fc900dfa26d2464606eb21df03
AnaliaGuari <85644984+AnaliaGuari@users.noreply.github.com>
2022-04-21 11:12:19 -0300

    Add files via upload

---
+++ Dia11/Ejercicio PairDia 11/Controllers/AutoController.java
@@ -0,0 +1,38 @@
+package com.bootcamp.EjerciciosDia11.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.websocket.server.PathParam;
+
+
+
+import static com.bootcamp.EjerciciosDia11.Services.AutoServices.*;
+
+@RestController
+@RequestMapping("/api")
+public class AutoController {
+    @PostMapping("/mostrar")
+    public String mostrarAutos() throws IOException {
+        return consultarTodos();
+    }
+
+    @PostMapping("/mostrar/{patente}")
+    public String buscarPorPatente(@PathParam("patente") String patente) throws IOException {
+        return consultarAuto(patente);
+    }
+
+
+    @GetMapping("/create")
+    public String crearAuto(@PathParam("modelo") String modelo,@PathParam("marca") String marca,
+                            @PathParam("patente") String patente,@PathParam("precio") float precio,
+                            @PathParam("cantPuertas") int cantPuertas ) throws IOException {
+        return registrarAuto(modelo, marca, patente, precio, cantPuertas);
+    }
+
+    @DeleteMapping("/delete/{patente}")
+    public String borrarPorPatente(@PathParam("patente") String patente) throws IOException {
+        return borrarAuto(patente);
+    }
+
+}
