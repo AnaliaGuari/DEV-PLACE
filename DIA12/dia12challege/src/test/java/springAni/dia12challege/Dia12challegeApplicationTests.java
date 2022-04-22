453ef78bfcd8dcb83ee08271c2547f17e57e8230
AnaliaGuari <85644984+AnaliaGuari@users.noreply.github.com>
2022-04-22 10:35:42 -0300

    Add files via upload

---
+++ DIA12/dia12challege/src/test/java/springAni/dia12challege/Dia12challegeApplicationTests.java
@@ -0,0 +1,13 @@
+package springAni.dia12challege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dia12challegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
